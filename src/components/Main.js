4ec7f16463058f97cb60a378eea6bacd797c8502
Your Name <you@example.com>
2018-09-05 23:55:13 +0100

    .

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter, IndexRoute, Link } from 'react-router-dom';
+import { HashRouter, Switch, Route, Link } from 'react-router-dom';
 
 import Home from './Home';
 import History from './History';
@@ -10,18 +10,18 @@ import Contacts from './Contacts';
 const Main = () => {
   return (
     <div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <div>
           <Header/>
           <Switch>
             <Route exact path={"/"} component={Home} />
-            <Route path={"/history"} component={History} />
-            <Route path={"/featured"} component={Contacts} />
-            <Route path="/contacts" component={Contacts} />
+            <Route path={"history"} component={History} />
+            <Route path={"featured"} component={Contacts} />
+            <Route path="contacts" component={Contacts} />
           </Switch>
           <Footer/>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
