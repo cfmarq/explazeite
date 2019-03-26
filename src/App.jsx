56d7bfba45ac99ee04e337c4c40614062bd2f6d7
Carlos Marques <cfmarq@cfmarq-MacBook-Pro.local>
2019-03-26 00:02:45 +0000

    capital correction for App.js

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/style.scss';
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+      <App />,document.getElementById('root')
+  )
